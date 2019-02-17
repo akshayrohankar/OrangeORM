e74022c0c7186e28f533e14af06708d19fdad621
Akshay Rohankar <akrohankar@gmail.com>
2019-02-17 17:56:43 +0530

    Admin page add user feature

---
+++ src/main/java/pageObjects/AdminPage.java
@@ -0,0 +1,85 @@
+package pageObjects;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import resources.base;
+
+public class AdminPage extends base {
+
+	public AdminPage(WebDriver driver, Properties prop) {
+		this.driver = driver;
+		this.prop = prop;
+	}
+
+	WebDriver driver;
+
+	By adminTab = By.xpath("//b[contains(text(),'Admin')]");
+	By addBtn = By.xpath("//input[@id='btnAdd']");
+	By empName = By.xpath("//input[@id='systemUser_employeeName_empName']");
+	By userName = By.xpath("//input[@id='systemUser_userName']");
+	By passWord = By.xpath("//input[@id='systemUser_password']");
+	By cnfpassWord = By.xpath("//input[@id='systemUser_confirmPassword']");
+	By saveBtn = By.xpath("//input[@id='btnSave']");
+
+	public WebElement getAdminTab() {
+		return driver.findElement(adminTab);
+	}
+	
+	public WebElement getAddbtn() {
+		return driver.findElement(addBtn);
+	}
+	
+	public WebElement getEmpName() {
+		return driver.findElement(empName);
+	}
+
+	public WebElement getUserName() {
+		return driver.findElement(userName);
+	}
+
+	public WebElement getPassWord() {
+		return driver.findElement(passWord);
+	}
+
+	public WebElement getCnfpassWord() {
+		return driver.findElement(cnfpassWord);
+	}
+
+	public WebElement getSaveBtn() {
+		return driver.findElement(saveBtn);
+	}
+
+	public void addUser() throws InterruptedException {
+		Random ran=new Random();
+		int ranvalue=ran.nextInt(1000);
+		getAdminTab().click();
+		getAddbtn().click();
+		getEmpName().sendKeys("a");
+		Thread.sleep(5000);
+		getEmpName().sendKeys(Keys.ENTER);
+		String usrname="Arnav"+ranvalue;
+		getUserName().sendKeys(usrname);
+		getPassWord().sendKeys("123456789");
+		getCnfpassWord().sendKeys("123456789");
+		Thread.sleep(5000);
+		getSaveBtn().click();
+	    List<WebElement> elementList=driver.findElements(By.xpath("//td[@class='left']//a"));
+		int count=elementList.size();
+		for(int i=0;i<count;i++)
+		{
+			if(elementList.get(i).equals(usrname))
+			{
+				System.out.println("User Added");
+			}
+		}
+		
+	}
+
+}
---
+++ src/test/java/testCases/LoginPageTest.java
@@ -0,0 +1,46 @@
+package testCases;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
+import resources.base;
+
+public class LoginPageTest extends base {
+
+	@BeforeTest
+	public void initMe() throws IOException {
+
+		initializeDriver();
+	}
+
+	@Test(enabled=false)
+	public void testLoginPage() throws InterruptedException {
+		LoginPage objLoginPage = new LoginPage(driver, prop);
+		objLoginPage.LoginProcess();
+
+	}
+
+	
+	@Test(enabled=false)
+	public void testValidateSuccessfullLogin() throws InterruptedException {
+		LoginPage objLoginPage = new LoginPage(driver, prop);
+		objLoginPage.LoginProcess();
+		objLoginPage.ValidateSuccessfullLogin();
+		String actual = objLoginPage.ValidateSuccessfullLogin();
+		assertEquals(actual, prop.getProperty("titlePage"));
+	}
+
+	@Test
+	public void testValidateUnSuccessfullLogin() throws InterruptedException {
+		LoginPage objLoginPage = new LoginPage(driver, prop);
+		objLoginPage.LoginProcess();
+		objLoginPage.ValidateUnSuccessfullLogin();
+		
+	}
+
+}
