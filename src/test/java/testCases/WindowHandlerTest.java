9897e2df84b0999f90dabe275fa734c81c71349e
Akshay Rohankar <akrohankar@gmail.com>
2019-11-06 21:33:37 +0530

    WindowsHandler test added

---
+++ src/test/java/testCases/WindowHandlerTest.java
@@ -0,0 +1,59 @@
+package testCases;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
+import resources.base;
+
+public class WindowHandlerTest extends base {
+
+	@BeforeTest
+	public void initMe() throws IOException {
+
+		initializeDriver();
+	}
+
+	@Test(priority = 0)
+	public void testLoginPage() throws InterruptedException {
+		LoginPage objLoginPage = new LoginPage(driver);
+		objLoginPage.LoginProcess();
+
+	}
+
+	@Test(priority = 1)
+	public void HandlerLoginPage() throws InterruptedException {
+		WebElement directoryTab = driver.findElement(By.xpath("//b[contains(text(),'Directory')]"));
+		WebElement maintenanceTab = driver.findElement(By.xpath("//b[contains(text(),'Maintenance')]"));
+
+		Actions action = new Actions(driver);
+		action.keyDown(Keys.CONTROL).build().perform();
+		directoryTab.click();
+		maintenanceTab.click();
+		action.keyUp(Keys.CONTROL).build().perform();
+		
+		Set<String> s1=driver.getWindowHandles();		
+        Iterator<String> it=s1.iterator();
+		
+        String mainWindow = it.next();
+        while(it.hasNext()) {
+        	
+        	driver.switchTo().window(it.next());
+        	System.out.println(driver.getTitle());
+        }
+        driver.switchTo().window(mainWindow);
+		
+	}
+
+
+}
